8b48f176ec3a555bd88df3d3fe6cf711788bfe53
atjoao <130053141+atjoao@users.noreply.github.com>
2024-01-20 14:00:50 +0000

    pequenos testes para isto

--- server/main.go
+++ server/main.go
@@ -6,10 +6,22 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func corsMiddleware() gin.HandlerFunc{
+	return func(ctx *gin.Context) {
+		ctx.Header("Access-Control-Allow-Origin", "*")
+        ctx.Header("Access-Control-Allow-Credentials", "true")
+        ctx.Header("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+        ctx.Header("Access-Control-Allow-Methods", "POST,HEAD,PATCH, OPTIONS, GET, PUT")
+
+		ctx.Next()
+	}
+}
+
 func main() {
 	app := gin.Default()
 	
 	r := app.Group("/test")
+	r.Use(corsMiddleware())
 	routers.TestGroup(r)
 
 	app.Run(":3000")
--- server/routers/testrouter.go
+++ server/routers/testrouter.go
@@ -1,13 +1,91 @@
 package routers
 
 import (
-	"net/http"
+	"fmt"
+	"io"
+	"os"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
 
 func TestGroup(rg *gin.RouterGroup) {
 	rg.GET("/a", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusOK, "hello world")
+		ctx.JSON(200, "hello world")
 	})
+
+	// la pq eu consigo defenir o tamanho do range que quero
+	// eu preciso de limitar ainda a velocidade
+	// eu posso tentar usar o buffer de audio e sincronizarlos
+	// stream e sempre mais rapido tmb
+	// mas eu fico sem habilidades de seek eu acho 
+
+	// eu tenho de ver isto 
+
+	/* 
+	rg.GET("/stream", func(ctx *gin.Context) {
+		chanStream := make(chan []byte)
+		ctx.Header("Content-Type", "audio/mp3")
+		ctx.Header("Content-Disposition", "inline; filename=audio")
+
+		go func() {
+			file, err := os.ReadFile("./audio/audio.opus")
+			//ctx.Header("Content-Length", string(bytes.Count(file, file)))
+
+			chanStream <- file
+			if err != nil {
+				ctx.String(500, "Error streaming file %s", err)
+			}
+			defer close(chanStream)
+		}()
+
+		ctx.Stream(func(w io.Writer) bool {
+			for buf := range chanStream {
+				w.Write(buf)
+			}
+			return false
+		})
+	}) 
+	*/
+
+	rg.GET("/stream2", func(ctx *gin.Context) {
+		// de alguma maneira isto funciona...
+		ctx.Header("Content-Type", "audio/ogg")
+		ctx.Header("Content-Disposition", "inline; filename=audio.opus")
+
+		data, err := os.ReadFile("./audio/audio.opus")
+		if err != nil {
+			ctx.String(500, "Error opening file: %s", err)
+			return
+		}
+
+		ctx.Header("Content-Length", strconv.Itoa(len(data)))
+
+		ctx.Stream(func(w io.Writer) bool {
+			var totalsent int64
+			var chunkSize int64 = 512 << 10
+
+			for totalsent < int64(len(data)) {
+				remaining := int64(len(data)) - totalsent
+				if remaining < chunkSize {
+					chunkSize = remaining
+				}
+
+				n, err := w.Write(data[totalsent : totalsent+chunkSize])
+				if err != nil {
+					fmt.Println("[stream] Err: ", err)
+					return false
+				}
+
+				totalsent += int64(n)
+			}
+
+			return false
+		})
+	})
+
+
+	// think abt this
+	// entendi como usar isto ig
+	rg.StaticFile("/audio", "./audio/audio.opus")
 }
