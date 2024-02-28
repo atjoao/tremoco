4619b24c812802250d03d27cdca7f1a821ae3315
atjoao <130053141+atjoao@users.noreply.github.com>
2024-02-28 01:13:43 +0000

    more changes

--- backend/env/env.go
+++ backend/env/env.go
@@ -1,3 +1,12 @@
 package env
 
 const SECRET_KEY = "hdfkjshfopasdpo"
+
+// oh well this is a secret key
+// i forgor to .gitignore this file
+// oh well i'll just change it later
+// probably to a random string
+// or something
+// i don't know
+// i'm just a comment
+// i don't know anything
