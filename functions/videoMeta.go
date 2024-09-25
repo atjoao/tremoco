276550d6353f4382e331399782e79725d02a1948
atjoao <130053141+atjoao@users.noreply.github.com>
2024-09-25 14:44:22 +0100

    fix links and requests

--- functions/videoMeta.go
+++ functions/videoMeta.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"io"
 	"net/http"
-	"net/url"
 	"strings"
 	"tremoco/utils"
 )
@@ -16,17 +15,28 @@ func VideoMeta(videoId string, includeVideo bool) (*utils.YT_VideoPlaybackRespon
 
 	inCache, getCacheValue := utils.StreamGetFromCache(videoId)
 	if !inCache {
-		const ytUrl string = "https://www.youtube.com/youtubei/v1/player?key=AIzaSyAO_FJ2SlqU8Q4STEHLGCilw_Y9_11qcW8"
-		var jsonStr = fmt.Sprintf(`{"videoId": "%s","context": {"client": {"clientName": "ANDROID_TESTSUITE","clientVersion": "1.9","androidSdkVersion": 30,"hl": "en","gl": "US","utcOffsetMinutes": 0}}}`, videoId)
+		const ytUrl string = "https://www.youtube.com/youtubei/v1/player?key=AIzaSyA8eiZmM1FaDVjRy-df2KTyQ_vz_yYM39w"
+		var jsonStr = fmt.Sprintf(`{"videoId": "%s","context": {"client": {"clientName": "ANDROID","clientVersion": "17.36.4","androidSdkVersion": 31,"hl": "en","gl": "US","utcOffsetMinutes": 0}}}`, videoId)
 
-		getVideoInfo, err := http.Post(ytUrl, "application/json", strings.NewReader(jsonStr))
+		getVideoInfo, err := http.NewRequest("POST", ytUrl, strings.NewReader(jsonStr))
 		if err != nil {
 			return nil, nil, err
 		}
 
-		defer getVideoInfo.Body.Close()
+		getVideoInfo.Header.Add("content-type", "application/json")
+		getVideoInfo.Header.Add("User-Agent", "com.google.android.youtube/17.36.4 (Linux; U; Android 12; GB) gzip")
 
-		getVideoBody, err := io.ReadAll(getVideoInfo.Body)
+		client := &http.Client{}
+		resp, err := client.Do(getVideoInfo)
+
+		if err != nil {
+			fmt.Println("Error sending request:", err)
+			return nil, nil, err
+		}
+
+		defer resp.Body.Close()
+
+		getVideoBody, err := io.ReadAll(resp.Body)
 		if err != nil {
 			return nil, nil, err
 		}
@@ -41,10 +51,10 @@ func VideoMeta(videoId string, includeVideo bool) (*utils.YT_VideoPlaybackRespon
 	}
 
 	for _, data := range response.StreamingData.AdaptiveFormats {
-		encodedUrl, err := url.QueryUnescape(data.URL)
+		/* encodedUrl, err := url.QueryUnescape(data.URL)
 		if err != nil {
 			return nil, nil, err
-		}
+		} */
 
 		if !includeVideo && data.AudioQuality == "" {
 			continue
@@ -52,7 +62,7 @@ func VideoMeta(videoId string, includeVideo bool) (*utils.YT_VideoPlaybackRespon
 
 		videoMeta := &utils.Streams{
 			AudioQuality: data.AudioQuality,
-			StreamUrl:    encodedUrl,
+			StreamUrl:    data.URL,
 			MimeType:     data.MimeType,
 		}
 
